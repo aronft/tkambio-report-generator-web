244ae718ed7a9a6f065f7a86afabdee615e773f9
Aron <blazte.aster@gmail.com>
2026-01-01 19:57:02 -0500

    feat(Container, Layout): add Container and Layout components with responsive styles

---
+++ src/core/components/objets/Container.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    tag?: 'div' | 'header' | 'section' | 'footer'
+  }>(),
+  {
+    tag: 'div',
+  },
+)
+</script>
+
+<template>
+  <component :is="tag" :class="[$attrs.class, 'container']"> <slot /> </component>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  max-width: variables.$mobile;
+  margin-inline: auto;
+  padding-inline: variables.$s-16;
+
+  @media (min-width: variables.$tablet) {
+    max-width: variables.$tablet;
+  }
+
+  @media (min-width: variables.$desktop) {
+    max-width: variables.$desktop;
+  }
+}
+</style>
---
+++ src/core/components/objets/Layout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Header from '../Header.vue'
+</script>
+
+<template>
+  <div v-bind:$attrs class="layout">
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  main {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
