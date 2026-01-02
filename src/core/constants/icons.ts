082fd8ef68c327b5dde95c827f0cded7b536f95f
Aron <blazte.aster@gmail.com>
2026-01-02 15:41:50 -0500

    chore: add close, download and progress-activity icons

--- src/core/constants/icons.ts
+++ src/core/constants/icons.ts
@@ -1,13 +1,17 @@
 import IconEye from '@/assets/icons/eye-show.svg?component'
 import IconEyeOff from '@/assets/icons/eye-hide.svg?component'
 import IconCalendar from '@/assets/icons/calendar.svg?component'
-// import IconDownload from '@/assets/icons/download.svg?component'
+import IconDownload from '@/assets/icons/download.svg?component'
+import Close from '@/assets/icons/close.svg?component'
+import ProgressActivity from '@/assets/icons/progress_activity.svg?component'
 
 export const ICON_LIBRARY = {
   eye: IconEye,
   'eye-off': IconEyeOff,
   calendar: IconCalendar,
-  // download: IconDownload,
+  download: IconDownload,
+  close: Close,
+  'progress-activity': ProgressActivity,
 } as const
 
 export type IconName = keyof typeof ICON_LIBRARY
