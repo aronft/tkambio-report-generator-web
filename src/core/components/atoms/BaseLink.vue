dd4b2c072a047bb61eafc90713aa5ca827721ddc
Aron <blazte.aster@gmail.com>
2026-01-02 15:42:21 -0500

    feat: add Link component

---
+++ src/core/components/atoms/BaseLink.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineOptions({
+  inheritAttrs: false,
+})
+</script>
+
+<template>
+  <a v-bind="$attrs" class="link">
+    <slot />
+  </a>
+</template>
+
+<style scoped lang="scss">
+.link {
+  color: currentColor;
+  text-decoration: none;
+}
+</style>
