9b5ab75bce8e35c3a4480698613eee4fb016df75
Aron <blazte.aster@gmail.com>
2026-01-01 19:59:39 -0500

    feat(LoginView): add InputPassword component and banner image to login form

---
+++ src/features/auth/components/InputPassword.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseIcon from './BaseIcon.vue'
+import BaseInput from './BaseInput.vue'
+
+const props = defineProps<{
+  label: string
+}>()
+
+const model = defineModel<string>({ required: true })
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const isShowingPassword = ref(false)
+
+function togglePassword() {
+  isShowingPassword.value = !isShowingPassword.value
+}
+</script>
+
+<template>
+  <BaseInput
+    v-model="model"
+    v-bind="$attrs"
+    :label="label"
+    :type="isShowingPassword ? 'text' : 'password'"
+  >
+    <template #suffix>
+      <button @click="togglePassword" type="button">
+        <BaseIcon :name="isShowingPassword ? 'eye-off' : 'eye'" size="20" />
+      </button>
+    </template>
+  </BaseInput>
+</template>
--- src/features/auth/views/LoginView.ts.vue
+++ src/features/auth/views/LoginView.ts.vue
@@ -1,3 +1,68 @@
+<script setup lang="ts">
+import BaseButton from '@/core/components/atoms/BaseButton.vue'
+import BaseInput from '@/core/components/atoms/BaseInput.vue'
+import InputPassword from '@/features/auth/components/InputPassword.vue'
+import Container from '@/core/components/objets/Container.vue'
+import Layout from '@/core/components/objets/Layout.vue'
+import { ref } from 'vue'
+
+const loginForm = ref({
+  email: '',
+  password: '',
+})
+</script>
+
 <template>
-    <h1>Login View</h1>
+  <Layout>
+    <Container tag="section" class="login">
+      <div class="login__promotion">
+        <img src="@/assets/images/banner-login.webp" alt="" />
+      </div>
+      <form class="login__form">
+        <h1 class="login__title">Inicia sesión</h1>
+        <BaseInput label="Correo" v-model="loginForm.email" type="email" />
+        <InputPassword label="Contraseña" v-model="loginForm.password" />
+        <BaseButton type="submit">Iniciar sesión</BaseButton>
+      </form>
+    </Container>
+  </Layout>
 </template>
+
+<style scoped lang="scss">
+.login {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: variables.$tablet) {
+    flex-direction: row;
+    gap: 4rem;
+  }
+
+  &__title {
+    margin-bottom: variables.$s-24;
+    text-align: center;
+  }
+
+  &__promotion {
+    display: none;
+    max-width: 28.5rem;
+
+    @media (min-width: variables.$tablet) {
+      display: block;
+      flex: 1;
+    }
+  }
+
+  &__form {
+    max-width: 28rem;
+    width: 100%;
+    display: flex;
+    gap: variables.$s-16;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
