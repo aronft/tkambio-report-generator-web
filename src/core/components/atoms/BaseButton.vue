abf8c5886e6c75bb83866aafff6be9310a1c56e3
Aron <blazte.aster@gmail.com>
2026-01-01 19:56:47 -0500

    feat(BaseButton): add component to render button

---
+++ src/core/components/atoms/BaseButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface Props {
+  variant?: 'secondary' | 'invalid'
+}
+withDefaults(defineProps<Props>(), {
+  variant: 'invalid',
+})
+</script>
+
+<template>
+  <button v-bind="$attrs" :class="['button', `button--${variant}`]"><slot /></button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  --button-bg-color: transparent;
+  --button-text-color: currentColor;
+  border: none;
+  background-color: var(--button-bg-color);
+  color: var(--button-text-color);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 3.125rem;
+  padding-block: 1rem;
+  padding-inline: 3rem;
+  font-size: variables.$fs-16;
+  font-weight: 400;
+
+  &--invalid {
+    --button-bg-color: var(--gray-color-100);
+    --button-text-color: var(--gray-color-900);
+  }
+
+  &--secondary {
+    --button-bg-color: var(--secondary-color);
+    --button-text-color: var(--gray-color-900);
+  }
+}
+</style>
